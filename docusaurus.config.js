5073f74ac6b524584e707995414795d82c500ad5
Constantin Minea <constantinminea@outlook.com>
2023-06-01 20:53:06 +0100

    edited tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'CApps',
-  tagline: 'Dinosaurs are cool!',
+  tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
